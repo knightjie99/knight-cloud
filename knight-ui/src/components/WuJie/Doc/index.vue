18d2b5dfcab1df9fd2611ec98601854f1b85b0a0
wujie <wuj@fise.com.cn>
2021-01-11 17:32:12 +0800

    初次提交

---
+++ knight-ui/src/components/WuJie/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WuJieDoc',
+  data() {
+    return {
+      url: 'http://doc.wujie.vip/wujie-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
