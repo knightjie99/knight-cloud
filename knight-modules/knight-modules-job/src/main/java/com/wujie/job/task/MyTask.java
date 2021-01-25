0cdbc23c05f89e9b6a92a6c82f56c1fcd55ab25c
wujie <wuj@fise.com.cn>
2021-01-25 17:05:02 +0800

    修改bug

--- knight-modules/knight-modules-job/src/main/java/com/wujie/job/task/MyTask.java
+++ knight-modules/knight-modules-job/src/main/java/com/wujie/job/task/MyTask.java
@@ -11,17 +11,17 @@ import com.wujie.common.core.utils.StringUtils;
 @Component("myTask")
 public class MyTask
 {
-    public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
+    public void myMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
         System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
     }
 
-    public void ryParams(String params)
+    public void myParams(String params)
     {
         System.out.println("执行有参方法：" + params);
     }
 
-    public void ryNoParams()
+    public void myNoParams()
     {
         System.out.println("执行无参方法");
     }
